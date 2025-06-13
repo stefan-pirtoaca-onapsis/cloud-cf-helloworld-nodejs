5eaadb6f4b7ab7ff611c2f3747e1a82d27dbe4e2
Stefan Pirtoaca <stefan.pirtoaca@onapsis.com>
2025-06-13 16:10:58 +0300

    Attempt to write to a file and read.

--- scripts/script.sh
+++ scripts/script.sh
@@ -5,3 +5,6 @@ set -e
 echo "--- Starting the custom script execution ---"
 echo "Current working directory is: $(pwd)"
 echo "Hello $1"
+touch newfile.txt
+echo "test" > newfile.txt
+cat newfile.txt
